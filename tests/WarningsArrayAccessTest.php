43e99d10867b61190c2f92acf52c2939e8044f17
Margus Kaidja <margusk@gmail.com>
2022-10-22 23:21:03 +0300

    Added ArrayAccess support to Warnings object. Added tests.

---
+++ tests/WarningsArrayAccessTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace margusk\Utils\Warbsorber\Tests;
+
+use BadMethodCallException;
+use OutOfRangeException;
+use margusk\Utils\Warbsorber\Entry;
+use margusk\Utils\Warbsorber\Warnings;
+use PHPUnit\Framework\TestCase;
+
+class WarningsArrayAccessTest extends TestCase
+{
+    public function testCorrectEntryKeyMustSucceed(): void
+    {
+        $expectedMessage = 'test error message';
+        $warnings = new Warnings([
+            new Entry(0, $expectedMessage, '', 0)
+        ]);
+
+        $this->assertEquals(true, isset($warnings[0]));
+        $this->assertEquals($expectedMessage, $warnings[0]->errStr);
+    }
+
+    public function testEntriesCantBeOverwritten(): void
+    {
+        $warnings = new Warnings([]);
+
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('warnings array can\'t be changed');
+
+        $warnings[1] = new Entry(0, '', '', 0);
+    }
+
+    public function testEntriesCantBeUnset(): void
+    {
+        $warnings = new Warnings([
+             new Entry(0, '', '', 0)
+        ]);
+
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('warnings array entries can\'t be unset');
+
+        unset($warnings[0]);
+    }
+
+    public function testInvalidEntryKeyMustTriggerException(): void
+    {
+        $warnings = new Warnings([]);
+
+        $offset = 10;
+        $this->expectException(OutOfRangeException::class);
+        $this->expectExceptionMessage('Undefined array key "' . $offset . '"');
+
+        $warnings[$offset];
+    }
+}
